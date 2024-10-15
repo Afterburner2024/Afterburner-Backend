7508168b6f20763792b0804eafb4d8191f208b70
dansun2 <dansun2@gmail.com>
2024-10-15 18:30:33 +0900

    rename: study package rename

--- src/main/java/com/afterburner/study/controller/StudyController.java
+++ src/main/java/com/afterburner/study/controller/StudyController.java
@@ -1,4 +1,10 @@
 package com.afterburner.study.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/study-group")
 public class StudyController {
+
 }
