d6969d134fb7aa9a996110d652825bb1137d7260
ppudding3861 <ppuding3861@gmail.com>
2024-10-20 02:42:02 +0900

    feat : login

--- src/main/java/com/afterburner/AfterburnerBackendApplication.java
+++ src/main/java/com/afterburner/AfterburnerBackendApplication.java
@@ -1,9 +1,12 @@
 package com.afterburner;
 
+import com.afterburner.oauth.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class AfterburnerBackendApplication {
 
 	public static void main(String[] args) {
