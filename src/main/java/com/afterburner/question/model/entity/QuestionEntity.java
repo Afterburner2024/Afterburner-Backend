52f3d910079eaa89b2e6b954b93375d54bcb0009
dansun2 <dansunmusik7@gmail.com>
2024-10-19 23:48:52 +0900

    style: question entity 오타수정

--- src/main/java/com/afterburner/question/model/entity/QuestionEntity.java
+++ src/main/java/com/afterburner/question/model/entity/QuestionEntity.java
@@ -1,10 +1,19 @@
 package com.afterburner.question.model.entity;
 
-import com.afterburner.common.enums.PostStatus;
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDateTime;
+import com.afterburner.common.enums.PostStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "question")
@@ -51,14 +60,14 @@ public class QuestionEntity {
 		this.questionImg = builder.questionImg;
 	}
 
-	public static class Builder{
+	public static class Builder {
 		private String questionTitle;
 		private String questionContent;
 		private PostStatus questionStatus;
 		private Integer questionUserId;
 		private String questionImg;
 
-		public Builder questionTitle(String questionTitle){
+		public Builder questionTitle(String questionTitle) {
 			if (questionTitle.length() > 30) {
 				throw new IllegalArgumentException("제목은 30자 미만만 가능합니다.");
 			} else {
@@ -67,7 +76,7 @@ public class QuestionEntity {
 			return this;
 		}
 
-		public Builder questionContent(String questionContent){
+		public Builder questionContent(String questionContent) {
 			if (questionContent.length() > 5000) {
 				throw new IllegalArgumentException("내용은 5000자 미만만 가능합니다.");
 			} else {
@@ -76,22 +85,22 @@ public class QuestionEntity {
 			return this;
 		}
 
-		public Builder questionStatus(PostStatus questionStatus){
+		public Builder questionStatus(PostStatus questionStatus) {
 			this.questionStatus = questionStatus;
 			return this;
 		}
 
-		public Builder questionUserId(Integer questionUserId){
+		public Builder questionUserId(Integer questionUserId) {
 			this.questionUserId = questionUserId;
 			return this;
 		}
 
-		public Builder questionImg(String questionImg){
+		public Builder questionImg(String questionImg) {
 			this.questionImg = questionImg;
 			return this;
 		}
 
-		public QuestionEntity build(){
+		public QuestionEntity build() {
 			if (questionTitle == null || questionContent == null) {
 				throw new IllegalArgumentException("모든 필드를 채워주세요.");
 			}
@@ -171,18 +180,28 @@ public class QuestionEntity {
 		this.questionImg = questionImg;
 	}
 
+	@PrePersist // 엔티티에 처음 저장될 때에만 동작함
+	public void prePersist() {
+		this.questionUpdatedAt = null; // 수정칸은 비어있도록
+	}
+
+	@PreUpdate // 엔티티가 수정될때 호출됨
+	public void preUpdate() {
+		this.questionUpdatedAt = LocalDateTime.now(); // 수정될때는 현재 시간으로 호출되게 함
+	}
+
 	@Override
 	public String toString() {
 		return "QuestionEntity{" +
-				"questionId=" + questionId +
-				", questionTitle='" + questionTitle + '\'' +
-				", questionContent='" + questionContent + '\'' +
-				", questionCreatedAt=" + questionCreatedAt +
-				", questionUpdatedAt=" + questionUpdatedAt +
-				", questionDeletedAt=" + questionDeletedAt +
-				", questionStatus=" + questionStatus +
-				", questionUserId=" + questionUserId +
-				", questionImg='" + questionImg + '\'' +
-				'}';
+			"questionId=" + questionId +
+			", questionTitle='" + questionTitle + '\'' +
+			", questionContent='" + questionContent + '\'' +
+			", questionCreatedAt=" + questionCreatedAt +
+			", questionUpdatedAt=" + questionUpdatedAt +
+			", questionDeletedAt=" + questionDeletedAt +
+			", questionStatus=" + questionStatus +
+			", questionUserId=" + questionUserId +
+			", questionImg='" + questionImg + '\'' +
+			'}';
 	}
 }
